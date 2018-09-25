c2e384d9bcffbc20fa05973777fe44c13876162c
River Merrill <rivermerrill.me@gmail.com>
2018-09-24 20:42:45 -0600

    number guessing using recursion

--- hellotest/Program.cs
+++ hellotest/Program.cs
@@ -6,7 +6,36 @@ namespace hellotest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random rndm = new Random();
+            int rand = rndm.Next(0, 100);
+            Console.WriteLine("I've chosen a number between 1 and 100.  Guess what it is.");
+            var guess = Console.ReadLine();
+            guessNumber(Int32.Parse(guess), rand);
+
+        }
+        static void guessNumber(int guess, int answer)
+        {
+            string newGuess;
+            if(guess > answer)
+            {
+                Console.WriteLine("Your guess is too high.  Try again.");
+                newGuess = Console.ReadLine();
+                guessNumber(Int32.Parse(newGuess), answer);
+            } else if (guess < answer)
+            {
+                Console.WriteLine("Your guess is too low.  Try again.");
+                newGuess = Console.ReadLine();
+                guessNumber(Int32.Parse(newGuess), answer);
+            } else if (guess == answer)
+            {
+                Console.WriteLine("Correct!!  Goodbye.");
+                Console.ReadLine();
+            } else
+            {
+                Console.WriteLine("What did you do???");
+                Console.ReadLine();
+            }
+
         }
     }
 }
